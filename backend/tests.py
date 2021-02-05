10012cd26f8995be3f369d2561e78400cd65af55
HOD101s <manasbass.99@gmail.com>
2021-02-05 20:40:47 +0530

    added tests to backend

--- backend/tests.py
+++ backend/tests.py
@@ -4,6 +4,7 @@ from app import app
 
 
 class APITests(unittest.TestCase):
+    # test GET /memes
     def test_get_memes(self):
         tester = app.test_client(self)
         response = tester.get('/memes')
@@ -11,6 +12,7 @@ class APITests(unittest.TestCase):
         assert response.status_code == 200
         assert len(data) <= 100
 
+    # test POST /memes
     def test_post_memes(self):
         tester = app.test_client(self)
         # test ivalid ur;
@@ -27,20 +29,50 @@ class APITests(unittest.TestCase):
         # test Valid upload
 
         # IMPORTANT: REPLACE url param with new image or change owner name
+        name = ''
+        url = ""
+        caption = ''
+        if name and url and caption:
+            response = tester.post(
+                '/memes', query_string=dict(name=name, url=url, caption=caption))
+            assert response.status_code == 200
+            assert 'id' in json.loads(response.data)
+            # test re-upload : should give 409
+            response = tester.post(
+                '/memes', query_string=dict(name=name, url=url, caption=caption))
+            assert response.status_code == 409
+            assert json.loads(response.data)[
+                'msg'] == "Entry already exists"
 
-        name = 'Office memer'
-        url = "https://i.chzbgr.com/full/9287716864/h9E975325/the-office-meme-face-opendulum-corporation-let-me-see-the-copier-again-get-out"
-        caption = 'Hank be Hank-in'
-        response = tester.post(
-            '/memes', query_string=dict(name=name, url=url, caption=caption))
+    # test GET /memes/<_id>
+    def test_get_meme_by_id(self):
+        tester = app.test_client(self)
+        # test with invalid id
+        _id = 'abcd'
+        response = tester.get(f'/memes/{_id}')
+        assert response.status_code == 404
+        # test with existing id
+        _id = '601d1514954ea510ec5396d6'
+        response = tester.get(f'/memes/{_id}')
         assert response.status_code == 200
-        assert 'id' in json.loads(response.data)
-        # test re-upload : should give 409
-        response = tester.post(
-            '/memes', query_string=dict(name=name, url=url, caption=caption))
+
+    # test PATCH /memes/<_id>
+    def test_patch_memes_by_id(self):
+        tester = app.test_client(self)
+
+        # update id
+        _id = '601d1514954ea510ec5396d6'
+
+        caption = "Office"  # existing caption
+        url = "https://i.pinimg.com/736x/90/bf/93/90bf93263e6378c4b8841873f98de89f.jpg"
+        response = tester.patch(
+            f'/memes/{_id}', data="{\"caption\" : \""+caption+"\",\"url\": \""+url+"\"}", headers={'Content-Type': 'text/plain'})
         assert response.status_code == 409
-        assert json.loads(response.data)[
-            'msg'] == "Entry already exists"
+
+        caption = "New Caption"  # new caption
+        response = tester.patch(
+            f'/memes/{_id}', data="{\"caption\" : \""+caption+"\",\"url\": \""+url+"\"}", headers={'Content-Type': 'text/plain'})
+        assert response.status_code == 200
 
 
 if __name__ == "__main__":
