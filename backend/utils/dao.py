fcce63adfa5135507ba3928e14bc87a9ac57f3cd
HOD101s <manasbass.99@gmail.com>
2021-02-06 16:21:55 +0530

    set dao datetime to utcnow

--- backend/utils/dao.py
+++ backend/utils/dao.py
@@ -27,8 +27,8 @@ class XmemeDb:
             'name': name,
             'url': url,
             'caption': caption,
-            'created': datetime.datetime.now(),
-            'updated': datetime.datetime.now()
+            'created': datetime.datetime.utcnow(),
+            'updated': datetime.datetime.utcnow()
         })
 
     def find_memes(self, sort=[], skip=0, limit=100):
@@ -49,7 +49,7 @@ class XmemeDb:
             _set['caption'] = caption
         if url:
             _set['url'] = url
-        _set['updated'] = datetime.datetime.now()
+        _set['updated'] = datetime.datetime.utcnow()
         self.db.memes.update_one({'_id': objectid.ObjectId(_id)}, {
             '$set': _set
         }, upsert=upsert)
