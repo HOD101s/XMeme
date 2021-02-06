b8398a80bc188111220caa621d888150fcfea837
HOD101s <manasbass.99@gmail.com>
2021-02-07 04:37:08 +0530

    added Contributors page

--- frontend/src/components/layouts/Contributors.js
+++ frontend/src/components/layouts/Contributors.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
 
 function Contributors() {
-  return <div></div>;
+  const [ownerNameData, setownerNameData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://xmeme-manas-api.herokuapp.com/contributors")
+      .then((resp) => {
+        setownerNameData(resp.data);
+      });
+  }, []);
+
+  const badge = ["primary", "secondary", "success", "danger", "warning"];
+  return (
+    <div>
+      <Container className="contributorsBadgeContainer">
+        {ownerNameData &&
+          ownerNameData.map((name, id) => (
+            <>
+              <h4 className="contributorsBadgeHeaderWrap">
+                <Badge variant={badge[id % badge.length]}>
+                  {name._id} <Badge variant="light">{name.count}</Badge>
+                </Badge>{" "}
+              </h4>
+            </>
+          ))}
+      </Container>
+    </div>
+  );
 }
 
 export default Contributors;
