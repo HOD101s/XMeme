13d1fbefcf090a2b20d14fd59ba384a693e1cd0a
HOD101s <manasbass.99@gmail.com>
2021-02-13 16:24:11 +0530

    updated docker_tests

--- docker_test.sh
+++ docker_test.sh
@@ -3,5 +3,24 @@ sudo apt install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 
-docker build -t xmeme_app .
-docker run -d -p 8081:8081 xmeme_app
+sudo docker build -t xmeme_app .
+sudo docker run -d -p 8081:8081 xmeme_app
+
+curl --location --request GET 'http://localhost:8081/memes'
+
+
+# Execute the POST /memes endpoint using curl
+
+curl --location --request POST 'http://localhost:8081/memes' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"name": "xyz",
+"url": "https://images.pexels.com/photos/3573382/pexels-photo-3573382.jpe",
+"caption": "Dhoni"
+}'
+
+# Execute the GET /memes endpoint using curl
+curl --location --request GET 'http://localhost:8081/memes'
+
+# If you have swagger enabled, make sure it is exposed at localhost:8081
+curl --location --request GET 'http://localhost:8081/swagger-ui/'
